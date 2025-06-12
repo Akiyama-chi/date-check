25c04ea5af09e04c896ab35b68377e6b213ba3d0
Akiyama <kamikaze031707@gmail.com>
2025-06-12 19:22:56 +0800

    Update time-check.js

--- time-check.js
+++ time-check.js
@@ -12,7 +12,7 @@ const options = {
   hour: '2-digit',
   minute: '2-digit',
   second: '2-digit',
-  hour12: true
+  hour12: false
 };
 
 const formatter = new Intl.DateTimeFormat('en-US', options);
@@ -29,7 +29,7 @@ const json = {
   day: parseInt(get('day')),
   year: parseInt(get('year')),
   time: `${hour}:${minute}:${second}`,
-  "24-hour": false
+  "24-hour": true
 };
 
 fs.writeFileSync('time.json', JSON.stringify(json, null, 2));
